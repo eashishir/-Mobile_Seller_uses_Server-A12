34537755e0c5bf767eb7e3d02cfd04c08257750f
shishir <eftekharAhmedshishir@gmail.com>
2022-11-30 14:16:27 +0600

    admin can delete user api done

--- index.js
+++ index.js
@@ -124,6 +124,14 @@ async function run() {
     res.send(users)
   });
 
+
+  app.delete('/users/:id',verifyJWT,verifyAdmin, async(req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await usersCollection.deleteOne(query);
+    res.send(result);
+  })
+
   // five step is isSeller check
 
   app.get('/users/seller/:email', async(req,res) => {
